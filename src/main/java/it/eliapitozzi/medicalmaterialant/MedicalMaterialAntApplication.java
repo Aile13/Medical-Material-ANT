cc1357c6964c78c32c1446e88f6d523e6573acc5
Aile <600eli@gmail.com>
2021-07-21 17:05:20 +0200

    Initial commit

---
+++ src/main/java/it/eliapitozzi/medicalmaterialant/MedicalMaterialAntApplication.java
@@ -0,0 +1,13 @@
+package it.eliapitozzi.medicalmaterialant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalMaterialAntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalMaterialAntApplication.class, args);
+	}
+
+}
