089ebedfaf576e1cee38363f04e968e231d62cb4
Aile <600eli@gmail.com>
2021-09-06 14:42:35 +0200

    Aggiunto opzione per Java 11.

--- src/main/java/it/eliapitozzi/medicalmaterialant/repository/TransactionRepository.java
+++ src/main/java/it/eliapitozzi/medicalmaterialant/repository/TransactionRepository.java
@@ -3,7 +3,6 @@ package it.eliapitozzi.medicalmaterialant.repository;
 import it.eliapitozzi.medicalmaterialant.model.Patient;
 import it.eliapitozzi.medicalmaterialant.model.Transaction;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
